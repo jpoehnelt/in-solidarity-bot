f42d2c231d2ccf027019f0d8467f947d175d6d5c
Justin <jpoehnelt@google.com>
2020-08-06 11:35:08 -0700

    init: github bot to provide status checks for inclusive language

---
+++ README.md
@@ -0,0 +1,32 @@
+# In Solidarity
+
+![Build](https://github.com/jpoehnelt/in-solidarity-bot/workflows/Build/badge.svg)
+![Release](https://github.com/jpoehnelt/in-solidarity-bot/workflows/Release/badge.svg)
+[![codecov](https://codecov.io/gh/jpoehnelt/in-solidarity-bot/branch/master/graph/badge.svg)](https://codecov.io/gh/jpoehnelt/in-solidarity-bot)
+![GitHub contributors](https://img.shields.io/github/contributors/jpoehnelt/in-solidarity-bot?color=green)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+
+> A GitHub Bot built with [Probot](https://github.com/probot/probot) that checks for inclusive language. See [help](./static/HELP.md) for more information.
+
+![](./static/screenshot.png)
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run typescript
+npm run build
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how the bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+This is not an official Google product.
---
+++ src/solidarity.ts
@@ -0,0 +1,178 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import gitDiffParser, { File } from "gitdiff-parser";
+
+import { Context } from "probot";
+import { LoggerWithTarget } from "probot/lib/wrap-logger";
+import { Octokit } from "@octokit/rest/";
+import fs from "fs";
+
+const PATTERN = /((?:(?:white|black)[_-]*list)|slave|master)/gi;
+const SUMMARY = fs.readFileSync("./static/HELP.md", "utf8");
+
+export enum Conclusion {
+  SUCCESS = "success",
+  FAILURE = "failure",
+  NEUTRAL = "neutral",
+  CANCELLED = "cancelled",
+  TIMED_OUT = "timed_out",
+  ACTION_REQUIRED = "action_required",
+}
+
+export class Solidarity {
+  private name = "Inclusive Language Check";
+  private context: Context;
+  private logger: LoggerWithTarget;
+  private checkId?: number;
+
+  constructor(context: Context, logger: LoggerWithTarget) {
+    this.context = context;
+    this.logger = logger;
+  }
+  get headSha(): string {
+    return this.context.payload.pull_request.head.sha;
+  }
+
+  get owner(): string {
+    return this.context.payload.repository.owner.login;
+  }
+
+  get repo(): string {
+    return this.context.payload.repository.name;
+  }
+  get checkOptions() {
+    return {
+      owner: this.owner,
+      repo: this.repo,
+      head_sha: this.headSha,
+      name: this.name,
+    };
+  }
+
+  async run() {
+    await this.start();
+    await this.update("in_progress");
+    const { conclusion, output } = await this.check();
+    await this.update("completed", conclusion, output);
+  }
+
+  async start() {
+    try {
+      const response = await this.context.github.checks.create({
+        ...this.checkOptions,
+        status: "queued",
+      });
+      this.checkId = response.data.id;
+    } catch (e) {
+      this.logger.warn(e);
+    }
+  }
+
+  async update(
+    status: "queued" | "in_progress" | "completed",
+    conclusion?: Conclusion,
+    output?: Octokit.ChecksUpdateParamsOutput,
+    details_url?: string
+  ) {
+    try {
+      await this.context.github.checks.update({
+        ...this.checkOptions,
+        check_run_id: this.checkId as number,
+        status,
+        conclusion,
+        ...(status === "completed" && {
+          completed_at: new Date().toISOString(),
+        }),
+        ...(output && { output }),
+        details_url,
+      });
+    } catch (e) {
+      this.logger.warn(e);
+    }
+  }
+
+  async check(): Promise<{
+    conclusion: Conclusion;
+    output: Octokit.ChecksUpdateParamsOutput;
+  }> {
+    let conclusion: Conclusion;
+    const output: Octokit.ChecksUpdateParamsOutput = {
+      title: "Inclusive Language Check",
+      summary: SUMMARY,
+    };
+
+    const { owner, repo } = this.checkOptions;
+    const response = await this.context.github.pulls.get({
+      owner,
+      repo,
+      pull_number: this.context.payload.number,
+      headers: { accept: "application/vnd.github.v3.diff" },
+    });
+
+    const diff = (response.data as unknown) as string;
+
+    const parsedDiff = gitDiffParser.parse(diff);
+
+    output.annotations = this.annotate(parsedDiff);
+
+    if (output.annotations.length) {
+      conclusion = Conclusion.NEUTRAL;
+      output.title = "Action Suggested";
+    } else {
+      output.title = "Success";
+      conclusion = Conclusion.SUCCESS;
+    }
+
+    this.logger.info({
+      conclusion,
+      repo,
+      owner,
+      pull_number: this.context.payload.number,
+      sha: this.headSha,
+    });
+
+    return { conclusion, output };
+  }
+
+  annotate(files: File[]): Octokit.ChecksUpdateParamsOutputAnnotations[] {
+    const annotations: Octokit.ChecksUpdateParamsOutputAnnotations[] = [];
+
+    for (const f of files) {
+      for (const h of f.hunks) {
+        for (const change of h.changes) {
+          if (change.isInsert || change.isNormal) {
+            // @ts-ignore
+            for (const match of change.content.matchAll(PATTERN)) {
+              annotations.push({
+                annotation_level: "warning",
+                end_column: match.index + match[0].length - 1,
+                end_line: change.lineNumber as number,
+                message: `Please consider an alternative to \`${match[0]}\`.`,
+                path: f.newPath,
+                raw_details: change.content,
+                start_column: match.index,
+                start_line: change.lineNumber as number,
+                title: "Match Found",
+              });
+            }
+          }
+        }
+      }
+    }
+    return annotations;
+  }
+}
