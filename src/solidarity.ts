e61241647c9e1fb5753d035769650b2617374583
Justin Poehnelt <jpoehnelt@google.com>
2020-08-14 15:22:43 -0600

    Update solidarity.ts

--- src/solidarity.ts
+++ src/solidarity.ts
@@ -91,6 +91,7 @@ export class Solidarity {
 
     try {
       this.config = await getConfig(this.context);
+      this.logger.info(config);
     } catch (e) {
       if (e instanceof InvalidConfigError) {
         conclusion = Conclusion.FAILURE;
